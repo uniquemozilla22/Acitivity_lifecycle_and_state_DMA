6ed52cd3c948842017adac50bcbc48f5af8ad738
uniquemozilla22 <uniqgaming26@gmail.com>
2021-03-17 00:23:56 +0545

    Coding Challenge completed

--- app/src/main/java/com/example/activities_and_intents_dma/MainActivity.java
+++ app/src/main/java/com/example/activities_and_intents_dma/MainActivity.java
@@ -8,48 +8,77 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String LOG_TAG = MainActivity.class.getSimpleName();
-    public static final String EXTRA_MESSAGE            = "com.example.android.activity_second.extra.MESSAGE";
-    public static final int TEXT_REQUEST = 1;
-
-    private EditText mMessageEditText;
-    private TextView mReplyHeadTextView;
-    private TextView mReplyTextView;
+    public static final int ITEM_REQUEST = 1;
+    private final TextView[] item = new TextView[10];
+    private ArrayList<String> itemsList = new ArrayList<>(10);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        item[0] = findViewById(R.id.item1);
+        item[1] = findViewById(R.id.item2);
+        item[2] = findViewById(R.id.item3);
+        item[3] = findViewById(R.id.item4);
+        item[4] = findViewById(R.id.item5);
+        item[5] = findViewById(R.id.item6);
+        item[6] = findViewById(R.id.item7);
+        item[7] = findViewById(R.id.item8);
+        item[8] = findViewById(R.id.item9);
+        item[9] = findViewById(R.id.item10);
 
-        mMessageEditText = findViewById(R.id.editText_main);
-        mReplyHeadTextView = findViewById(R.id.text_header_reply);
-        mReplyTextView = findViewById(R.id.text_message_reply);
-    }
-
-
-    public void launchSecondActivity(View view) {
-        Log.d(LOG_TAG, "Button clicked!");
-        Intent intent = new Intent(this, SecondActivity.class);
-        String message = mMessageEditText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivityForResult(intent, TEXT_REQUEST);
+        if (savedInstanceState != null) {
+            itemsList = savedInstanceState.getStringArrayList("ItemsListStringArray");
+            int i;
+            if (itemsList != null && itemsList.size() > 0) {
+                for (i = 0; i < itemsList.size(); i++) {
+                    if (itemsList.size() > 10) {
+                        Toast.makeText(MainActivity.this, "Cannot add more items to the list", Toast.LENGTH_LONG).show();
+                        break;
+                    }
+                    item[i].setVisibility(View.VISIBLE);
+                    item[i].setText(itemsList.get(i));
+                }
+            }
+        }
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (itemsList.size() != 0) {
+            outState.putStringArrayList("ItemsListStringArray", itemsList);
+        }
+    }
+
+    public void addItem(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivityForResult(intent, ITEM_REQUEST);
+    }
+
+    // method for when SecondActivity returns intent
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == TEXT_REQUEST) {
+        if (requestCode == ITEM_REQUEST) {
             if (resultCode == RESULT_OK) {
-                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+                String itemString = data.getStringExtra(SecondActivity.EXTRA_ITEMS);
+                itemsList.add(itemString);
+                int i;
+                for (i = 0; i < itemsList.size(); i++) {
+                    if (itemsList.size() > 10) {
+                        Toast.makeText(MainActivity.this, "Cannot add more items to the list", Toast.LENGTH_LONG).show();
+                        break;
+                    }
+                    item[i].setVisibility(View.VISIBLE);
+                    item[i].setText(itemsList.get(i));
+                }
 
-                mReplyHeadTextView.setVisibility(View.VISIBLE);
-
-                mReplyTextView.setText(reply);
-                mReplyTextView.setVisibility(View.VISIBLE);
             }
         }
     }
--- app/src/main/java/com/example/activities_and_intents_dma/SecondActivity.java
+++ app/src/main/java/com/example/activities_and_intents_dma/SecondActivity.java
@@ -5,33 +5,56 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
-
-    public static final String EXTRA_REPLY =
-            "com.example.android.activity_second.extra.REPLY";
-    private EditText mReply;
+    public static final String EXTRA_ITEMS = "com.example.activities_and_intents_dma.extra.ITEMS";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        mReply = findViewById(R.id.editText_second);
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        TextView textView = findViewById(R.id.text_message);
-        textView.setText(message);
     }
 
+    public void selectItem(View view) {
+        Intent return_items_intent = new Intent();
 
-    public void returnReply(View view) {
-        String reply = mReply.getText().toString();
+        switch (view.getId()){
+            case R.id.button_chocolate:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Chocolate"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            case R.id.button_oil:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Oil"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            case R.id.button_milk:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Milk"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            case R.id.button_eggs:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Eggs"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            case R.id.button_cheese:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Cheese"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            case R.id.button_rice:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Rice"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            case R.id.button_wheat:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Wheat Flour"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            case R.id.button_bread:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Bread"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            case R.id.button_sugar:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Sugar"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            case R.id.button_salt:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Salt"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            default:
+                Toast.makeText(SecondActivity.this, "There was an error", Toast.LENGTH_LONG).show();
+                break;
+        }
 
-        Intent replyIntent = new Intent();
-        replyIntent.putExtra(EXTRA_REPLY, reply);
-        setResult(RESULT_OK, replyIntent);
-        finish();
     }
+
 }
