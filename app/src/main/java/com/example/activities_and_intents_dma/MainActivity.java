3517e8b106ef3a36cb06132efd1ac722aa4b7437
uniquemozilla22 <uniqgaming26@gmail.com>
2021-03-17 12:33:04 +0545

    hOMEWORK COMPLETED

--- app/src/main/java/com/example/activities_and_intents_dma/MainActivity.java
+++ app/src/main/java/com/example/activities_and_intents_dma/MainActivity.java
@@ -6,51 +6,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String LOG_TAG = MainActivity.class.getSimpleName();
-    public static final String EXTRA_MESSAGE            = "com.example.android.activity_second.extra.MESSAGE";
-    public static final int TEXT_REQUEST = 1;
-
-    private EditText mMessageEditText;
-    private TextView mReplyHeadTextView;
-    private TextView mReplyTextView;
+    private TextView countTextView;
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mMessageEditText = findViewById(R.id.editText_main);
-        mReplyHeadTextView = findViewById(R.id.text_header_reply);
-        mReplyTextView = findViewById(R.id.text_message_reply);
+        countTextView = findViewById(R.id.count_textView);
+        if (savedInstanceState != null){
+            String i = savedInstanceState.getString("count_value");
+            countTextView.setText(i);
+            count = Integer.parseInt(i);
+        }
     }
 
-
-    public void launchSecondActivity(View view) {
-        Log.d(LOG_TAG, "Button clicked!");
-        Intent intent = new Intent(this, SecondActivity.class);
-        String message = mMessageEditText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivityForResult(intent, TEXT_REQUEST);
+    public void increment(View view) {
+        count ++;
+        countTextView.setText(String.valueOf(count));
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == TEXT_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
-
-                mReplyHeadTextView.setVisibility(View.VISIBLE);
-
-                mReplyTextView.setText(reply);
-                mReplyTextView.setVisibility(View.VISIBLE);
-            }
-        }
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("count_value", countTextView.getText().toString());
     }
+
 }
